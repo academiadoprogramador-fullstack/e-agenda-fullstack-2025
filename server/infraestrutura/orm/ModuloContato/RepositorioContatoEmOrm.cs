e9a6beb79ada3b7972ce9669c309ea7a4d1e6357
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-09-02 14:15:06 -0300

    Finalizados métodos do controlador

--- server/infraestrutura/orm/ModuloContato/RepositorioContatoEmOrm.cs
+++ server/infraestrutura/orm/ModuloContato/RepositorioContatoEmOrm.cs
@@ -1,7 +1,15 @@
 ﻿using eAgenda.Core.Dominio.ModuloContato;
 using eAgenda.Infraestrutura.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
 
 namespace eAgenda.Infraestrutura.Orm.ModuloContato;
 
-public class RepositorioContatoEmOrm(AppDbContext contexto)
-    : RepositorioBaseEmOrm<Contato>(contexto), IRepositorioContato;
+public class RepositorioContatoEmOrm(AppDbContext contexto) : RepositorioBaseEmOrm<Contato>(contexto), IRepositorioContato
+{
+    public override async Task<Contato?> SelecionarRegistroPorIdAsync(Guid idRegistro)
+    {
+        return await registros
+            .Include(c => c.Compromissos)
+            .FirstOrDefaultAsync(x => x.Id == idRegistro);
+    }
+}
--- server/web-api/Controllers/ContatoController.cs
+++ server/web-api/Controllers/ContatoController.cs
@@ -1,6 +1,5 @@
-using eAgenda.Core.Aplicacao.ModuloContato.Cadastrar;
-using eAgenda.Core.Dominio.ModuloContato;
-using eAgenda.WebApi.Models.ModuloContato.Cadastrar;
+using eAgenda.Core.Aplicacao.ModuloContato.Commands;
+using eAgenda.WebApi.Models.ModuloContato;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,25 +7,10 @@ namespace eAgenda.WebApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ContatoController : ControllerBase
+public class ContatoController(IMediator mediator) : ControllerBase
 {
-    private readonly IMediator mediator;
-    private readonly IRepositorioContato repositorioContato;
-    private readonly ILogger<ContatoController> logger;
-
-    public ContatoController(
-        IMediator mediator,
-        IRepositorioContato repositorioContato,
-        ILogger<ContatoController> logger
-    )
-    {
-        this.mediator = mediator;
-        this.repositorioContato = repositorioContato;
-        this.logger = logger;
-    }
-
     [HttpPost]
-    public async Task<IActionResult> Cadastrar(CadastrarContatoRequest request)
+    public async Task<ActionResult<CadastrarContatoResponse>> Cadastrar(CadastrarContatoRequest request)
     {
         var command = new CadastrarContatoCommand(
              request.Nome,
@@ -43,14 +27,91 @@ public class ContatoController : ControllerBase
 
         var response = new CadastrarContatoResponse(result.Value.Id);
 
-        return Created(string.Empty, response);
+        return CreatedAtAction(nameof(SelecionarRegistroPorId), new { contatoId = result.Value.Id }, response);
+    }
+
+    [HttpPut("{contatoId:guid}")]
+    public async Task<ActionResult<EditarContatoResponse>> Editar(Guid contatoId, EditarContatoRequest request)
+    {
+        var command = new EditarContatoCommand(
+            contatoId,
+            request.Nome,
+            request.Telefone,
+            request.Email,
+            request.Empresa,
+            request.Cargo
+        );
+
+        var result = await mediator.Send(command);
+
+        if (result.IsFailed)
+            return BadRequest();
+
+        var response = new EditarContatoResponse(
+            result.Value.Id,
+            result.Value.Nome,
+            result.Value.Telefone,
+            result.Value.Email,
+            result.Value.Empresa,
+            result.Value.Cargo
+        );
+
+        return Ok(response);
+    }
+
+    [HttpDelete("{contatoId:guid}")]
+    public async Task<ActionResult<EditarContatoResponse>> Excluir(Guid contatoId)
+    {
+        var command = new ExcluirContatoCommand(contatoId);
+
+        var result = await mediator.Send(command);
+
+        if (result.IsFailed)
+            return BadRequest();
+
+        return NoContent();
     }
 
     [HttpGet]
-    public async Task<IActionResult> SelecionarRegistros()
+    public async Task<ActionResult<SelecionarContatosResponse>> SelecionarRegistros(
+        [FromQuery] SelecionarContatosRequest? selecionarContatosRequest
+    )
     {
-        var registros = await repositorioContato.SelecionarRegistrosAsync();
+        var query = new SelecionarContatosQuery(selecionarContatosRequest?.Quantidade);
 
-        return Ok(registros);
+        var result = await mediator.Send(query);
+
+        if (result.IsFailed)
+            return BadRequest();
+
+        var response = new SelecionarContatosResponse(
+            result.Value.Contatos.Count,
+            result.Value.Contatos
+        );
+
+        return Ok(response);
+    }
+
+    [HttpGet("{contatoId:guid}")]
+    public async Task<ActionResult<SelecionarContatosResponse>> SelecionarRegistroPorId(Guid contatoId)
+    {
+        var query = new SelecionarContatoPorIdQuery(contatoId);
+
+        var result = await mediator.Send(query);
+
+        if (result.IsFailed)
+            return NotFound(contatoId);
+
+        var response = new SelecionarContatoPorIdResponse(
+            result.Value.Id,
+            result.Value.Nome,
+            result.Value.Telefone,
+            result.Value.Email,
+            result.Value.Empresa,
+            result.Value.Cargo,
+            result.Value.Compromissos
+        );
+
+        return Ok(response);
     }
 }
